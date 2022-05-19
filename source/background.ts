265d0450be9e2adff862911ff661a36e48eb5281
ivebencrazy <git@bpev.me>
2022-05-19 14:31:52 -0700

    add favicon replacement

--- source/background.ts
+++ source/background.ts
@@ -1,3 +1,7 @@
+/**
+ * Watches tabs and manages
+ */
+
 import browserAPI from './utilities/browserAPI.ts';
 
 browserAPI.tabs.onUpdated.addListener(() => {
--- source/contentScript.ts
+++ source/contentScript.ts
@@ -1,5 +1,22 @@
-/** */
-
 /// <reference lib="dom" />
 
+/**
+ * Check siteList and ignoreList from chrome storage
+ * Use those to determine if we should override favicon
+ * Override favicon if applicable
+ */
+import browserAPI from './utilities/browserAPI.ts';
+import { appendFaviconLink, removeAllFaviconLinks } from "./utilities/faviconHelpers.js";
+
 console.log('hello!');
+
+browserAPI.storage.sync.get(['siteList', 'ignoreList'], (result: any) => {
+  const url = new URL(location.href)
+  console.log(url);
+  if (result.siteList.some((site: string) => location.href.match(site))) {
+    console.log('replace!');
+    removeAllFaviconLinks();
+    appendFaviconLink('😀', true);
+    console.log('replaced with 😀');
+  }
+});
