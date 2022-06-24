982b785d80c134720500584416d18af067691424
Ben Pevsner <git@bpev.me>
2022-06-23 19:46:03 -0700

    fix: update about page

--- source/pages/about_page.tsx
+++ source/pages/about_page.tsx
@@ -14,11 +14,15 @@ export default function () {
       <h2>Privacy Policy</h2>
       <p>
         Favioli does not track you, does not transmit any data outside of your
-        computer, and does not modify anything outside of favicons.
+        computer, and does not modify anything on websites outside of favicons.
       </p>
       <p>
-        We are actively trying to respect your privacy. The source code is
-        freely available for inspection and/or to build from source:
+        The only links to external sites/etc are informational links on this
+        about page.
+      </p>
+      <p>
+        I am actively trying to respect your privacy. The source code is freely
+        available for inspection and/or to build from source:
       </p>
       <a href='https://github.com/ivebencrazy/favioli'>
         https://github.com/ivebencrazy/favioli
@@ -26,20 +30,31 @@ export default function () {
 
       <h2>Permissions</h2>
       <p>
-        Favioli tries to make permissions granular for whatever you are trying
-        to use it for. To replace a favicon, we do run our own code on websites
-        you are browsing. So if you are very nervous about that, you can enable
-        Favioli to only run on specific websites.
+        To replace favicons, Favioli runs code on websites that you are
+        browsing. If you are very nervous about that, some browsers now allow
+        you to restrict extensions to only run on specific websites. As features
+        are added, I will try to keep permissions as granular as possible.
       </p>
 
       <h2>Something is broken!</h2>
       <p>
         Please report any bugs you find on{' '}
-        <a href='https://github.com/ivebencrazy/favioli/issues'>Github</a>! In
-        the future, we will try to add a button you can use to send this kind of
-        thing. Since we do not collect any automated feedback, though, your
+        <a href='https://github.com/ivebencrazy/favioli/issues'>
+          Github Issues
+        </a>! Since Favioli does not collect any automated feedback, any kind of
         feedback is very useful!
       </p>
+      <p>
+        In the future, I will add a form to{' '}
+        <a href='https://favioli.com/contact'>favioli.com/contact</a>{' '}
+        so that sending feedback will be easier.
+      </p>
+
+      <p>Thanks for using Favioli!</p>
+      <p>
+        – <a href='https://bpev.me'>Ben</a>
+      </p>
+      <p></p>
     </Fragment>
   );
 }
